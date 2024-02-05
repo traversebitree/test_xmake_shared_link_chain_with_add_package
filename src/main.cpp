b5f4eed83b60fd5ac5511778a40c47fd045c25b4
Kong <xykong421@outlook.com>
2024-02-05 22:49:13 +0800

    Update xmake.lua

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <dylibsrc.hpp>
+// #include <fmt/core.h>
 #include <iostream>
-
 
 using namespace std;
 
 int main(int argc, char **argv) {
   cout << "hello world!" << endl;
   print_calc_plus_ab(3, 4);
+  // fmt::print("{}\n", "Print from fmt...");
   return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,34 @@
 add_rules("mode.debug", "mode.release")
 set_languages("cxx20")
+add_rules("set_export_all_symbols")
+add_rules("add_pkgenv_when_linking")
+
+rule("set_export_all_symbols")
+do
+  on_load(function(target)
+    if target:kind() == "static" then
+      target:set("kind", "static")
+    elseif target:kind() == "shared" then
+      target:set("kind", "shared")
+      if is_plat("windows") and target:toolchains()[1]:config("vs") then
+        import("core.project.rule")
+        local rule = rule.rule("utils.symbols.export_all")
+        target:rule_add(rule)
+        target:extraconf_set("rules", "utils.symbols.export_all", { export_classes = true })
+      end
+    end
+  end)
+end
+rule_end()
+
+rule("add_pkgenv_when_linking")
+do
+  before_link(function(target)
+    os.addenvs(target:pkgenvs())
+  end
+  )
+end
+rule_end()
 
 add_requires("fmt 10.x", {
 	debug = is_mode("debug"),
@@ -22,6 +51,6 @@ do
 	add_files("dylibsrc/dylibsrc.cpp")
 	add_includedirs("dylibsrc/", { public = true })
 	add_headerfiles("dylibsrc/dylibsrc.hpp", { install = true })
-	add_packages("fmt", { public = true })
+	add_packages("fmt", { public = false })
 end
 target_end()
