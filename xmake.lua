baa446924389453f0a26d0a38d029b6895f05db9
traversebitree <traversebitree@users.noreply.github.com>
2024-02-05 01:00:02 +0000

    Automated commit of stylua-action changes.

--- xmake.lua
+++ xmake.lua
@@ -2,26 +2,26 @@ add_rules("mode.debug", "mode.release")
 set_languages("cxx20")
 
 add_requires("fmt 10.x", {
-  debug = is_mode("debug"),
-  configs = {
-    shared = true,
-  },
+	debug = is_mode("debug"),
+	configs = {
+		shared = true,
+	},
 })
 
 target("main")
 do
-    set_kind("binary")
-    add_files("src/main.cpp")
-    add_deps("mydylib")
+	set_kind("binary")
+	add_files("src/main.cpp")
+	add_deps("mydylib")
 end
 target_end()
 
 target("mydylib")
 do
-    set_kind("shared")
-    add_files("dylibsrc/dylibsrc.cpp")
-    add_includedirs("dylibsrc/", { public = true })
-    add_headerfiles("dylibsrc/dylibsrc.hpp", { install = true})
-    add_packages("fmt")
+	set_kind("shared")
+	add_files("dylibsrc/dylibsrc.cpp")
+	add_includedirs("dylibsrc/", { public = true })
+	add_headerfiles("dylibsrc/dylibsrc.hpp", { install = true })
+	add_packages("fmt")
 end
 target_end()
