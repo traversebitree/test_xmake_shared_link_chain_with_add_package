af4f7e4f57bf64557698f8817d85525b6f4dc96f
Kong <xykong421@outlook.com>
2024-02-06 10:19:10 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,58 +1,85 @@
 add_rules("mode.debug", "mode.release")
 set_languages("cxx20")
 add_rules("set_export_all_symbols")
-add_rules("add_pkgenv_when_linking")
+add_rules("copy_dll_when_build")
 
 rule("set_export_all_symbols")
 do
-	on_load(function(target)
-		if target:kind() == "static" then
-			target:set("kind", "static")
-		elseif target:kind() == "shared" then
-			target:set("kind", "shared")
-			if is_plat("windows") and target:toolchains()[1]:config("vs") then
-				import("core.project.rule")
-				local rule = rule.rule("utils.symbols.export_all")
-				target:rule_add(rule)
-				target:extraconf_set("rules", "utils.symbols.export_all", { export_classes = true })
-			end
-		end
-	end)
+    on_load(
+        function(target)
+            if target:kind() == "static" then
+                target:set("kind", "static")
+            elseif target:kind() == "shared" then
+                target:set("kind", "shared")
+                if is_plat("windows") and target:toolchains()[1]:config("vs") then
+                    import("core.project.rule")
+                    local rule = rule.rule("utils.symbols.export_all")
+                    target:rule_add(rule)
+                    target:extraconf_set("rules", "utils.symbols.export_all", {export_classes = true})
+                end
+            end
+        end
+    )
 end
 rule_end()
 
-rule("add_pkgenv_when_linking")
+rule("copy_dll_when_build")
 do
-	before_link(function(target)
-		os.addenvs(target:pkgenvs())
-	end)
-	after_link(function(target)
-		print(os.getenvs())
-	end)
+    after_link(
+        function(target)
+            installed_libfiles = installed_libfiles or {}
+            for _, pkg in ipairs(target:orderpkgs()) do
+                local target_build_dir = target:targetdir()
+                if pkg:enabled() and pkg:get("libfiles") then
+                    for _, dll_path in ipairs(table.wrap(pkg:get("libfiles"))) do
+                        if
+                            (target:is_plat("windows", "mingw") and dll_path:endswith(".dll")) or
+                                ((not target:is_plat("windows", "mingw")) and
+                                    (dll_path:endswith(".so") or dll_path:match(".+%.so%..+$") or
+                                        dll_path:endswith(".dylib")))
+                         then
+                            if not installed_libfiles[dll_path] then
+                                local dll_name = path.filename(dll_path)
+                                local targe_dll_path = path.join(target_build_dir, dll_name)
+                                if os.isfile(targe_dll_path) then
+                                  os.rm(targe_dll_path)
+                                end
+                                os.vcp(dll_path, target_build_dir, {symlink = true, force = true})
+                                installed_libfiles[dll_path] = true
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    )
 end
 rule_end()
 
-add_requires("fmt 10.x", {
-	debug = is_mode("debug"),
-	configs = {
-		shared = true,
-	},
-})
+add_requires(
+    "fmt 10.x",
+    {
+        debug = is_mode("debug"),
+        configs = {
+            shared = true
+        }
+    }
+)
 
 target("main")
 do
-	set_kind("binary")
-	add_files("src/main.cpp")
-	add_deps("mydylib")
+    set_kind("binary")
+    add_files("src/main.cpp")
+    add_deps("mydylib")
 end
 target_end()
 
 target("mydylib")
 do
-	set_kind("shared")
-	add_files("dylibsrc/dylibsrc.cpp")
-	add_includedirs("dylibsrc/", { public = true })
-	add_headerfiles("dylibsrc/dylibsrc.hpp", { install = true })
-	add_packages("fmt", { public = false })
+    set_kind("shared")
+    add_files("dylibsrc/dylibsrc.cpp")
+    add_includedirs("dylibsrc/", {public = true})
+    add_headerfiles("dylibsrc/dylibsrc.hpp", {install = true})
+    add_packages("fmt", {public = false})
 end
 target_end()
